2fa1b9c52a6182f4e5d5f18c102d94138d6d8fc6
Ignacio Lizarralde <lizarralde.ignacio@gmail.com>
2018-07-21 13:07:48 -0300

    Changed ip provider new favicon

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,10 +4,11 @@ import {
        } from './types';
 
 const VERIFY_URL = 'https://us-central1-mywebsite-8a1f0.cloudfunctions.net/verifyCaptcha';
-const GET_IP_URL = 'https://freegeoip.net/json/';
+const GET_IP_URL = 'https://api.ipstack.com/check';
+const IPSTACK_ACCESS_KEY = 'YOUR KEY';
 
 export function verifyReCaptcha(response) {
-  const request = axios.get(`${GET_IP_URL}`).then(ipResponse => {
+  const request = axios.get(`${GET_IP_URL}?access_key=${IPSTACK_ACCESS_KEY}`).then(ipResponse => {
     return axios.get(`${VERIFY_URL}?response=${response}&remoteip=${ipResponse.data.ip}`);
   });
   return {
